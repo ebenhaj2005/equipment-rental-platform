99d131fa1c8f4e862f8bbf2099fd8f0e8d2ab29c
ebenhaj2005 <145969715+ebenhaj2005@users.noreply.github.com>
2025-12-30 15:09:14 +0100

    Initial Spring Boot project setup with dependencies

---
+++ src/test/java/com/rentalplatform/equipmentrental/EquipmentRentalPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentalplatform.equipmentrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentRentalPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
